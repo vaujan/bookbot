513416cbb4d7473bcceda02e1dcb3779a15b8146
vaujan <fauzan20014@gmail.com>
2024-11-06 09:36:56 +0700

    fixing conflicts from previous commit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f'{num_words} words founded in the book.')
+    
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f: 
+        return f.read()
+    
+main()
