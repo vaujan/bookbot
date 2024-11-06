817b77b1564bad56b3de22837745420cc6611423
vaujan <fauzan20014@gmail.com>
2024-11-06 14:31:23 +0700

    checkpoint: add book report, finalized

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ def main():
     lowered_text = text.lower()
     words = get_words(lowered_text)
     chararcters_number = get_num_characters(words)
-    sorted_charachters_number = dict(sorted(chararcters_number.items()))
-    return print(f'List of the characters number are: {sorted_charachters_number}')
+    sorted_charachters_number = sorted(chararcters_number, reverse=True, key=lambda k: k['count'])
 
+    print(f'"--- Begin report of {book_path} ---"')
+    print(f'{len(words)} was found in the document')
+    print ()
+    for charachter in sorted_charachters_number: 
+        print(f"The '{charachter["letter"]}' was found {charachter["count"]} times")
+    print ()
+    print ()
 
 def get_book_text(path):
     with open(path) as f:
@@ -15,17 +21,23 @@ def get_book_text(path):
 def get_words(text):
     words = text.split()
     return words
-
 def get_num_characters(list):
-    charachters_count = {}
+    characters_count_list = []
 
     for word in list:
-        for char in word:
-            if char not in charachters_count:
-                charachters_count[char] = 1
-            else:
-                charachters_count[char] += 1
+        for letter in word:
+            if letter.isalpha():
+                found = False
+                for char_dict in characters_count_list: 
+                    if char_dict["letter"] == letter:
+                        char_dict["count"] += 1
+                        found = True
+                        break
+                if not found:
+                    characters_count_list.append({"letter": letter, "count": 1})
+    return characters_count_list
 
-    return charachters_count
+def sort_on(dict):
+    return dict["count"]
 
 main()
